36861bf9ee6546e469a4eba07aa43ea317b84d65
Сергей Владиславович Маклецов <smak.80@yandex.ru>
2025-05-12 15:05:29 +0300

    Перенос текста при вводе

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,42 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
 @Preview
 fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Content(Modifier.fillMaxWidth())
+    }
+}
+
+@Composable
+fun Content(
+    modifier: Modifier = Modifier
+){
+    var text by remember { mutableStateOf("") }
+    Column(modifier) {
+        OutlinedTextField(
+            value = text,
+            onValueChange = { text = it },
+            modifier = Modifier.fillMaxWidth(),
+        )
+        Button(onClick = {}){
+            Text("Нажать нежно!")
         }
+        Text(text)
     }
 }
 
